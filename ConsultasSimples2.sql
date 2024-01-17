59ac8fe47e8e63e5282011077c1e3d238acb557d
MDCpedro <pedromdc101@gmail.com>
2024-01-17 20:41:18 +0100

    Base de datos

---
+++ ConsultasSimples2.sql
@@ -0,0 +1,11 @@
+-- Consulta els noms dels llibres i la seva editorial
+SELECT(titol)
+-- Repeteix la consulta anterior excloent l'editorial Planeta
+
+--• Consulta la concatenació del nom i l’autor de cada llibre, separats per ‘ - ‘.
+
+--• Consulta els autors, però separant-los en noms i llinatges. Considera que estan separats pel primer espai.
+
+--• Consulta, retornant-ho en majúscula, els 10 primers caràcters de cada autor. Abans, elimina els espais del nom.
+
+--• Repeteix l’anterior consulta, però restringint-la a autors amb 12 caràcters o més i eliminant també els punts del nom.
