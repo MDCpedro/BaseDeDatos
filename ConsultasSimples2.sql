2a0d358f2ff45a5d21bba79d9039651ecec8107e
MDCpedro <pedromdc101@gmail.com>
2024-01-19 18:52:15 +0100

    Act basededatos

--- ConsultasSimples2.sql
+++ ConsultasSimples2.sql
@@ -1,7 +1,10 @@
 -- Consulta els noms dels llibres i la seva editorial
-SELECT(titol)
+SELECT ll.titol, ed.nom FROM llibre, editorial ed WHERE ll.id_editorial = ed.id;
+SELECT ll.titol, ed.nom FROM llibre ll JOIN editorial ed ON ll.id_editorial = ed.id;
 -- Repeteix la consulta anterior excloent l'editorial Planeta
-
+SELECT titol, id_editorial
+FROM llibre
+HAVING editorial <> 'Planeta';
 --• Consulta la concatenació del nom i l’autor de cada llibre, separats per ‘ - ‘.
 
 --• Consulta els autors, però separant-los en noms i llinatges. Considera que estan separats pel primer espai.
